8583c1124773bc77b1276af4b58394f7e3a12027
debych <zh.eliseev@yandex.ru>
2023-02-03 13:07:34 +0300

    add deploy and update scripts for Symbiosis

---
+++ script/UpdateSymbiosisFacet.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { UpdateScriptBase } from "./utils/UpdateScriptBase.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import { DiamondCutFacet, IDiamondCut } from "rubic/Facets/DiamondCutFacet.sol";
+import { SymbiosisFacet } from "rubic/Facets/SymbiosisFacet.sol";
+
+contract DeployScript is UpdateScriptBase {
+    using stdJson for string;
+
+    function run() public returns (address[] memory facets) {
+        string memory path = string.concat(root, "/deployments/", network, ".", fileSuffix, "json");
+
+        string memory json = vm.readFile(path);
+        address facet = json.readAddress(".SymbiosisFacet");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Symbiosis
+        if (loupe.facetFunctionSelectors(facet).length == 0) {
+            bytes4[] memory exclude;
+            cut.push(
+                IDiamondCut.FacetCut({
+                    facetAddress: address(facet),
+                    action: IDiamondCut.FacetCutAction.Add,
+                    functionSelectors: getSelectors("SymbiosisFacet", exclude)
+                })
+            );
+            cutter.diamondCut(cut, address(0), "");
+        }
+
+        facets = loupe.facetAddresses();
+
+        vm.stopBroadcast();
+    }
+}
