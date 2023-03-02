55b12357a10280e4cd25cbc449327ba73fd45e84
debych <zh.eliseev@yandex.ru>
2023-03-02 13:41:10 +0300

    add UpdateMultichainMappings.s.sol

---
+++ script/management/UpdateMultichainMappings.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { UpdateScriptBase } from "../utils/UpdateScriptBase.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import { MultichainFacet } from "rubic/Facets/MultichainFacet.sol";
+
+contract DeployScript is UpdateScriptBase {
+    using stdJson for string;
+
+    function run() public {
+        string memory path = string.concat(root, "/config/multichainTokens.json");
+        string memory json = vm.readFile(path);
+        bytes memory anyMappingsRaw = json.parseRaw(
+            string.concat(".", network)
+        );
+        MultichainFacet.AnyMapping[] memory anyMappings = abi.decode(
+            anyMappingsRaw,
+            (MultichainFacet.AnyMapping[])
+        );
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        MultichainFacet(diamond).updateAddressMappings(anyMappings);
+
+        vm.stopBroadcast();
+    }
+}
