d68563abb38383333a96c8af0984789f4b0d52ab
debych <zh.eliseev@yandex.ru>
2023-05-11 15:49:27 +0300

    remove redundant code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -31,10 +31,6 @@ const config: HardhatUserConfig = {
       },
     ],
   },
-  namedAccounts: {
-    deployer: 0,
-    simpleERC20Beneficiary: 1,
-  },
   networks: {
     hardhat: {
       chainId: 1337,
