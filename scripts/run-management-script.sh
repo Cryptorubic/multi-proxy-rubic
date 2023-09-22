e44224182b12d47459b19b96d4e4537320ba5735
root <root@kek.rofl>
2023-09-22 14:31:15 +0300

    fix network log

--- scripts/run-management-script.sh
+++ scripts/run-management-script.sh
@@ -12,7 +12,7 @@ run() {
 
   NETWORK=$(cat ./networks | gum filter --placeholder "Network")
 
-  echo 'running on $NETWORK...'
+  echo "running on ${NETWORK}..."
 
   SCRIPT=$(ls -1 script/management/ | sed -e 's/\.s.sol$//' | gum filter --placeholder "Deploy Script")
 
