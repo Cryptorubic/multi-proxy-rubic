96a3275cbaabd78c9d79f4922f977bf68bd653b6
root <root@kek.rofl>
2024-04-12 12:14:10 +0300

    change checkFailure in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,10 +19,10 @@ deploy() {
 	echo $SCRIPT
 
 	RAW_RETURN_DATA=$(SALT=$SALT NETWORK=$NETWORK FILE_SUFFIX=$FILE_SUFFIX forge script script/$SCRIPT.s.sol -f $NETWORK --json --broadcast --skip-simulation --silent --legacy)
+  checkFailure
   echo $RAW_RETURN_DATA
 	CLEAN_RETURN_DATA=$(echo $RAW_RETURN_DATA | sed 's/^.*{\"logs/{\"logs/')
 	echo $CLEAN_RETURN_DATA | jq 2> /dev/null
-	#checkFailure
 	RETURN_DATA=$(echo $CLEAN_RETURN_DATA | jq -r '.returns' 2> /dev/null)
 
 	deployed=$(echo $RETURN_DATA | jq -r '.deployed.value')
